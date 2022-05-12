d645fe29cc4dbcbf0504060f94e582d8f65e55cc
Heitor Victor <heitor.victor1996@gmail.com>
2022-05-11 23:40:32 -0300

    Api Rest
    Criando uma api rest para o novo sistema

---
+++ api.rest/src/test/kotlin/com/agv/engdb/api/rest/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.agv.engdb.api.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
