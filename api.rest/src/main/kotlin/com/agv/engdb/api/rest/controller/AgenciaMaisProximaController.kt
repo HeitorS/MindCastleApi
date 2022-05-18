9de02e4f5946026883e4caa5c9d8fc485f24dd5f
Heitor Victor <heitor.victor1996@gmail.com>
2022-05-17 23:34:59 -0300

    Agência mais próxima
    Criando API das agências mais próximas

---
+++ api.rest/src/main/kotlin/com/agv/engdb/api/rest/controller/AgenciaMaisProximaController.kt
@@ -0,0 +1,9 @@
+package com.agv.engdb.api.rest.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/agencia-mais-proxima")
+class AgenciaMaisProximaController {
+}
